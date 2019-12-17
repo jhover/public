090b546fdd9d4ce9a61d1679e7f8f6bc5d8ff772
jhover <jhover@library1.cshl.edu>
2019-12-17 11:55:58 -0500

    Update.

--- misc/inherit.py
+++ misc/inherit.py
@@ -16,17 +16,13 @@ import argparse
 from configparser import ConfigParser
 import logging
 
-
 class Plugin(object):
-
     REPR_ATTRS=[]
-
 
     def __init__(self, config):
         self.config = config
         self.kname = self.__class__.__name__
-        self.log = logging.getLogger(self.kname)
-        
+        self.log = logging.getLogger(self.kname)      
 
     def getinfo(self):
         self.log.debug("getinfo called...")
@@ -38,7 +34,6 @@ class Plugin(object):
         for atr in self.__class__.REPR_ATTRS:
             s += " %s=%s" % (atr, self.__getattribute__(atr))
         return s 
-
         
 
 class FooPlugin(Plugin):
@@ -51,7 +46,6 @@ class FooPlugin(Plugin):
         self.log.debug("lookup in config file: %s" % self.configname)
         # can't get self.kname until after init(), but logging gets class name OK. 
         #self.log.debug("%s initialized...") % self.kname
-
 
     def execute(self, input):
         self.log.info("Executing on %s" % input)
@@ -71,54 +65,14 @@ class BarPlugin(Plugin):
         self.special = special
         self.extra = self.config.get(self.configname, 'extra')
    
-
     def execute(self, input):
         self.log.info("Executing on %s" % input)
         output = input + self.special
         self.log.debug("Returning output.")
         return output
 
-
-if __name__ == '__main__':
-    
-    #FORMAT='%(asctime)s (UTC) [ %(levelname)s ] %(name)s %(filename)s:%(lineno)d %(funcName)s(): %(message)s'
-    FORMAT='%(asctime)s (UTC) [ %(levelname)s ] %(filename)s:%(lineno)d %(name)s.%(funcName)s(): %(message)s'
-    logging.basicConfig(format=FORMAT)
-    
-    parser = argparse.ArgumentParser()
-      
-    parser.add_argument('-d', '--debug', 
-                        action="store_true", 
-                        dest='debug', 
-                        help='debug logging')
-
-    parser.add_argument('-v', '--verbose', 
-                        action="store_true", 
-                        dest='verbose', 
-                        help='verbose logging')
-
-    parser.add_argument('special', 
-                        metavar='special', 
-                        type=str,
-                        nargs='?', 
-                        help='a special arg for BarPlugins')
-    
-    parser.add_argument('-c', '--config', 
-                        action="store", 
-                        dest='conffile', 
-                        default='~/etc/cafa4.conf',
-                        help='Config file path [~/etc/cafa4.conf]')
-    
-    args= parser.parse_args()
-    
-    if args.debug:
-        logging.getLogger().setLevel(logging.DEBUG)
-    if args.verbose:
-        logging.getLogger().setLevel(logging.INFO)
-    
-    cp = ConfigParser()
-    cp.read(args.conffile)
-    
+def runtest(config):
+    cp = config    
     plp = Plugin(cp)
     print("plugin repr is %s " % str(plp))
     foo = FooPlugin(cp)
@@ -129,4 +83,41 @@ if __name__ == '__main__':
     print( foo.execute("zazzo"))
     print(bar.execute("wazzo-"))
     print("bar repr is %s" % str(bar))
+    
+
+
+if __name__ == '__main__':
+
+    FORMAT='%(asctime)s (UTC) [ %(levelname)s ] %(filename)s:%(lineno)d %(name)s.%(funcName)s(): %(message)s'
+    logging.basicConfig(format=FORMAT)    
+    parser = argparse.ArgumentParser()      
+    parser.add_argument('-d', '--debug', 
+                        action="store_true", 
+                        dest='debug', 
+                        help='debug logging')
+    parser.add_argument('-v', '--verbose', 
+                        action="store_true", 
+                        dest='verbose', 
+                        help='verbose logging')
+    parser.add_argument('special', 
+                        metavar='special', 
+                        type=str,
+                        nargs='?', 
+                        help='a special arg for BarPlugins')   
+    parser.add_argument('-c', '--config', 
+                        action="store", 
+                        dest='conffile', 
+                        default='~/etc/cafa4.conf',
+                        help='Config file path [~/etc/cafa4.conf]')
+    args= parser.parse_args()
+    
+    if args.debug:
+        logging.getLogger().setLevel(logging.DEBUG)
+    if args.verbose:
+        logging.getLogger().setLevel(logging.INFO)
+    
+    cp = ConfigParser()
+    cp.read(args.conffile)
+
+    runtest(cp)
     
